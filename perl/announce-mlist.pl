550ac75a37892fecf318e7578e2bf707b3419ac6
Michael Wagner <michael@wagnertech.de>
2018-01-01 23:49:48 +0100

    epic

--- perl/announce-mlist.pl
+++ perl/announce-mlist.pl
@@ -5,13 +5,33 @@ use IO::Socket::UNIX;
 # Configuration
 our $SOCK_PATH = "/var/run/mlist.sock";
 
+my $usage = "$0 <list-name> <permission>\n <permission>: all|list|<.perm>";
+
+# check command line
+die $usage if ($#ARGV < 1);
+my ($list, $perm) = @ARGV;
+
+# check list name
+die "list name must end with .mlist" unless $list =~ /.*\.mlist/;
+die "list not existing" unless -f $list;
+
+# check permission
+die "invalid permission expression" unless
+	$perm eq "all"
+	|| $perm eq "list"
+	|| -f $perm;
+
 my $client = IO::Socket::UNIX->new(
-#	Type => SOCK_STREAM,
-	Type => SOCK_DGRAM,
+	Type => SOCK_STREAM,
+#	Type => SOCK_DGRAM,
 	Peer => $SOCK_PATH,
 );
 
 die "Can't create socket: $!" unless $client;
 
-$client->send("Hallo\n");
+$client->send("A,$list,$perm\n");
     
+chomp (my $ans = <$client>);
+
+die "daemon returned $ans" unless $ans eq "0";
+
--- perl/mlistd.pl
+++ perl/mlistd.pl
@@ -4,30 +4,53 @@ use IO::Socket::UNIX;
 
 # Configuration
 our $SOCK_PATH = "/var/run/mlist.sock";
+our $USER_DIR = "/home/mmail";
+
 unlink $SOCK_PATH if -e $SOCK_PATH;
 umask(0000);
 # define server:
-#my $server = IO::Socket::UNIX->new(
-#    Type => SOCK_STREAM,
-#    Local => $SOCK_PATH,
-#    Listen => 1,
-#);
 my $server = IO::Socket::UNIX->new(
-    Type => SOCK_DGRAM,
+    Type => SOCK_STREAM,
     Local => $SOCK_PATH,
     Listen => 1,
 );
+#my $server = IO::Socket::UNIX->new(
+#    Type => SOCK_DGRAM,
+#    Local => $SOCK_PATH,
+#    Listen => 1,
+#);
 
 die "Can't create socket: $!" unless $server;
 
-#while (my $conn = $server->accept())
-#{
+while (my $conn = $server->accept())
+{
 #	while (my $line = <$conn>) {
 #    	print("$line");# . "\n");
 #	}
-#}
-my $msg;
-while (1) {
-	$server->recv($msg,64);
-	print("$msg");
+	my $line = <$conn>;
+	my $ret = 0;
+	
+	if ($line eq "U") {
+		
+		# update list
+		$ret = system("newaliases");
+	}
+	else {
+		my ($cmd,$list,$perm) = split(/,/,$line);
+		if ($cmd eq "A") {
+			system("grep -v \"^$list\" /etc/aliases > /tmp/aliases");
+			system("mv /tmp/aliases /etc/aliases");
+			system("echo \"$list: :include:$USER_DIR/$list\" >> /etc/aliases" );
+		}
+		else {
+			$ret = 1;
+		}  
+		
+	}
+	print $conn "$ret\n";
 }
+#my $msg;
+#while (1) {
+#	$server->recv($msg,64);
+#	print("$msg");
+#}
