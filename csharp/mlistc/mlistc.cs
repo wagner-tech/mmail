c86c3bd27e62eeb549f2ddd4d185894e1a7c62bb
Sparky6 <sparky6@core>
2023-08-09 21:46:03 +0200

    csharp-s6g

--- csharp/mlistc/mlistc.cs
+++ csharp/mlistc/mlistc.cs
@@ -1,21 +1,27 @@
 using System;
+
+using MListc;
 using mutil;
 
 class MListClient {
-    static string[] pars = {"XmlFile", "Template", "Server"};
-    static string read_inkey() {
+    static string[] pars = {"XmlFile", "Template", "Server", "Browser"};
+    static string read_inkey(bool interactive) {
         var configs = Configuration.getInstance();
         string value;
         for (int i=0; i<pars.Length; i++) {
             value = configs.Get(pars[i], "-- not set --");
             Console.WriteLine("({0}) {1} : {2}", i, pars[i], value);
         }
-        Console.WriteLine("Enter parameter number to edit (empty for END)");
-        return Console.ReadLine();
+        string ret = String.Empty;
+        if (interactive) {
+            Console.WriteLine("Enter parameter number to edit (empty for END)");
+            ret = Console.ReadLine();
+        }
+        return ret;
     } 
     static int edit_config() {
         var configs = Configuration.getInstance();
-        string inkey = read_inkey();
+        string inkey = read_inkey(true);
         while (inkey != "") {
             int i = Convert.ToInt32(inkey);
             if (i<0 || i>= pars.Length) Console.WriteLine("Invalid parameter number");
@@ -24,47 +30,69 @@ class MListClient {
                 string inval = Console.ReadLine();
                 configs.Add(pars[i], inval);
             }
-            inkey = read_inkey();
+            inkey = read_inkey(true);
         }
         return 0;
     }
-    static int MainMenu() {
+    static void Dispatch(int selection) {
+        switch (selection) {
+            case 0: break;
+            case 1: read_inkey(false);
+                break;
+            case 2: edit_config();
+                break;
+            case 3: int rc = MListC.create_local_html();
+                if (rc == 0) Console.WriteLine("Lokales HTML erzeugt.");
+                else Console.WriteLine("create_local_html returned: {0}", new ReturnCode(rc).ToString());
+                break;
+            case 4: MListC.show_local_html();
+                break;
+            default: Console.WriteLine("Fehlerhafte Eingabe: "+Convert.ToString(selection));
+                break;
+        }
+    }
+    static void MainMenu() {
         int auswahl = 0;
         do {
             // show basic configuration
-            string xml_file = Configuration.getInstance().Get("XmlFile");
-            if (xml_file == null) xml_file = "";
-            string html_tpl = Configuration.getInstance().Get("Template");
-            if (html_tpl == null) html_tpl = "";
+            string xml_file = Configuration.getInstance().Get("XmlFile", "-- not set --");
+            string html_tpl = Configuration.getInstance().Get("Template", "-- not set --");
 
             // main loop
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("XmlFile: "+xml_file+" ; Template: "+html_tpl);
             Console.WriteLine();
-            Console.WriteLine("(1) : Konfiguration ändern");
-            Console.WriteLine("(2) : Lokales HTML erzeugen");
-            Console.WriteLine("(3) : Lokales HTML anzeigen");
-            Console.WriteLine("(4) : HTML auf Server laden");
-            Console.WriteLine("(5) : Testmail verschicken");
-            Console.WriteLine("(6) : Newsletter verschicken");
+            Console.WriteLine("(1) : Konfiguration anzeigen");
+            Console.WriteLine("(2) : Konfiguration ändern");
+            Console.WriteLine("(3) : Lokales HTML erzeugen");
+            Console.WriteLine("(4) : Lokales HTML anzeigen");
+            Console.WriteLine("(5) : Bilder auf Server laden");
+            Console.WriteLine("(6) : Testmail verschicken");
+            Console.WriteLine("(7) : Newsletter verschicken");
             Console.WriteLine("(0) : Programm beenden");
             string eingabe = Console.ReadLine();
             auswahl = Convert.ToInt32(eingabe);
-            
-            switch (auswahl) {
-                case 0: break;
-                case 1: edit_config();
-                    break;
-                default: Console.WriteLine("Unknown input: "+Convert.ToString(auswahl));
-                    break;
-            }
+            Dispatch(auswahl);
         } while (auswahl != 0);
-        return 0;
     }
-    static int Main() {
-        Console.WriteLine("mMail Client");
-        // load configuration
-        Configuration.getInstance(".mlistc");
-        MainMenu();
+    static int Main(string[] args) {
+        try {
+            Console.WriteLine("mMail Client");
+            // load configuration
+            Configuration.getInstance(".mlistc");
+            // set message strategy
+            MessageTool.getInstance().MessageToolImpl = new ConsoleMessageTool();
+
+            if (args.Length == 0) MainMenu();
+            else {
+                int selection = Convert.ToInt32(args[0]);
+                Dispatch(selection);
+            }
+        }
+        catch (System.Exception e) {
+            Console.WriteLine("Ein Fehler ist aufgetreten: {0}", e.Message);
+            return 1;
+        }
 
         return 0;
     }
--- debian/mlistc.cp
+++ debian/mlistc.cp
@@ -3,9 +3,8 @@ set -e
 
 BUILD_DIR=~/build
 mkdir -p $1/usr/lib/mlistc
-#cp $BUILD_DIR/csharp/mutil/mutil.dll $1/usr/lib/mdoc-tools
-#cp $BUILD_DIR/csharp/mDocLib/mdoclib.dll $1/usr/lib/mdoc-tools
 cp $BUILD_DIR/csharp/mlistc/mlistc.exe $1/usr/lib/mlistc
+cp $BUILD_DIR/csharp/mListDll/mlist.dll $1/usr/lib/mlistc
 mkdir -p $1/usr/bin
 cp csharp/bin/mlistc $1/usr/bin
 
